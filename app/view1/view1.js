9c4631c8a3d3bb493629c6efca9827d4b3955af8
Bill Chen <billchen@aol.com>
2015-09-08 15:42:57 -0700

    Minor edits to make it work

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,11 +2,15 @@
   'use strict';
    
   angular
-    .module('myApp.view1', ['ngRoute', 'formly', 'formlyBootstrap', 'ngMessages'])
+    .module('myApp.view1', [
+      'ngRoute', 
+      'formly', 
+      'formlyBootstrap', 
+      'ngMessages'])
     .config(routerConfig)
     .controller('View1Controller', controller);
       
-  routerConfig.$inject('$routeProvider');        
+  routerConfig.$inject = ['$routeProvider'];        
   function routerConfig($routeProvider) {
       $routeProvider.when('/view1', {
           templateUrl: 'view1/view1.html',
@@ -17,59 +21,57 @@
         
   function controller() {
       var viewModel = this;
-
-      viewModel.formData = {};
-
-      viewModel.formFields = [
-          {
-              key: 'username',
-              type: 'input',
-              templateOptions: {
-                  type: 'text',
-                  label: 'User Name',
-                  placeholder: 'john smith',
-                  required: true,
-                  description: 'User Name for the system',
-                  minlength: 2
-              }
-          }, {
-              key: 'password',
-              type: 'input',
-              templateOptions: {
-                  type: 'password',
-                  label: 'Password',
-                  required: true,
-                  disabled: false,
-                  hideExpression: '!username'
-              }
-          }, {
-              "key": "love",
-              "type": "input",
-              templateOptions: {
-                  type: 'number',
-                  "label": "How much love?",
-                  "min": 0,
-                  "max": 100,
-                  "maxlength": 4,
-                  "required": true
-              }
-          }, {
-              type: 'input',
-              templateOptions: {
-                  label: 'random number',
-                  type: 'number',
-                  maxlength: 12
-              }
+      var fields = [
+        {
+          key: 'username',
+          type: 'input',
+          templateOptions: {
+              type: 'text',
+              label: 'User Name',
+              placeholder: 'john smith',
+              required: true,
+              description: 'User Name for the system',
+              minlength: 2
           }
+        }, {
+          key: 'password',
+          type: 'input',
+          templateOptions: {
+              type: 'password',
+              label: 'Password',
+              required: true,
+              disabled: false,
+              hideExpression: '!username'
+          }
+        }, {
+          "key": "love",
+          "type": "input",
+          templateOptions: {
+              type: 'number',
+              "label": "How much love?",
+              "min": 0,
+              "max": 100,
+              "maxlength": 4,
+              "required": true
+          }
+        }, {
+          type: 'input',
+          templateOptions: {
+              label: 'random number',
+              type: 'number',
+              maxlength: 12
+          }
+        }
       ];
-
-      viewModel.formOptions = {
-          //uniqueFormId: 'myFormId'
-      };
-
-      viewModel.onSubmit = function() {
+      
+      function submit() {
           console.log('form submitted:', viewModel.formData);
       };
+      
+      viewModel.formData = {};
+      viewModel.formOptions = {};
+      viewModel.onSubmit = submit;
+      viewModel.formFields = fields;
   };   
 })()
 
