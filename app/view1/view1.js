52f280467705888974754f898f3adb6f84e28232
Bill Chen <billchen@aol.com>
2015-09-08 13:42:11 -0700

    Updated project to focus specifically on Angular Formly - Removed Bower and specifically only use NPM

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,21 +1,70 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'aa.formExtensions', 'aa.notify'])
+angular.module('myApp.view1', ['ngRoute', 'formly', 'formlyBootstrap', 'ngMessages'])
 
 .config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.when('/view1', {
             templateUrl: 'view1/view1.html',
-            controller: 'View1Ctrl',
-            controllerAs: 'View1'
+            controller: 'View1Controller',
+            controllerAs: "View1"
         });
 }])
 
-.controller('View1Ctrl', [function () {
-    var viewModel = this;
-    
-    viewModel.onSubmit = function Submit() {
-        console.log('form submitted:', viewModel.number);
-    };
+.controller('View1Controller', [
+    function () {
+        var viewModel = this;
 
+        viewModel.formData = {};
+
+        viewModel.formFields = [
+            {
+                key: 'username',
+                type: 'input',
+                templateOptions: {
+                    type: 'text',
+                    label: 'User Name',
+                    placeholder: 'john smith',
+                    required: true,
+                    description: 'User Name for the system',
+                    minlength: 2
+                }
+            }, {
+                key: 'password',
+                type: 'input',
+                templateOptions: {
+                    type: 'password',
+                    label: 'Password',
+                    required: true,
+                    disabled: false,
+                    hideExpression: '!username'
+                }
+            }, {
+                "key": "love",
+                "type": "input",
+                templateOptions: {
+                    type: 'number',
+                    "label": "How much love?",
+                    "min": 0,
+                    "max": 100,
+                    "maxlength": 4,
+                    "required": true
+                }
+            }, {
+                type: 'input',
+                templateOptions: {
+                    label: 'random number',
+                    type: 'number',
+                    maxlength: 12
+                }
+            }
+        ];
+
+        viewModel.formOptions = {
+            //uniqueFormId: 'myFormId'
+        };
+
+        viewModel.onSubmit = function() {
+            console.log('form submitted:', viewModel.formData);
+        };
 }]);
