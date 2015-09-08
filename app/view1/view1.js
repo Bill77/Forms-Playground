d6befb170f6cf4faa97c960b6abb7fa06d0718b6
Bill Chen <billchen@aol.com>
2015-09-08 14:56:09 -0700

    Updated to John Papa Style Guide - added settings.json for VS Code

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,70 +1,76 @@
-'use strict';
+(function() {
+  'use strict';
+   
+  angular
+    .module('myApp.view1', ['ngRoute', 'formly', 'formlyBootstrap', 'ngMessages'])
+    .config(routerConfig)
+    .controller('View1Controller', controller);
+      
+  routerConfig.$inject('$routeProvider');        
+  function routerConfig($routeProvider) {
+      $routeProvider.when('/view1', {
+          templateUrl: 'view1/view1.html',
+          controller: 'View1Controller',
+          controllerAs: "View1"
+      });
+  };
+        
+  function controller() {
+      var viewModel = this;
 
-angular.module('myApp.view1', ['ngRoute', 'formly', 'formlyBootstrap', 'ngMessages'])
+      viewModel.formData = {};
 
-.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'view1/view1.html',
-            controller: 'View1Controller',
-            controllerAs: "View1"
-        });
-}])
+      viewModel.formFields = [
+          {
+              key: 'username',
+              type: 'input',
+              templateOptions: {
+                  type: 'text',
+                  label: 'User Name',
+                  placeholder: 'john smith',
+                  required: true,
+                  description: 'User Name for the system',
+                  minlength: 2
+              }
+          }, {
+              key: 'password',
+              type: 'input',
+              templateOptions: {
+                  type: 'password',
+                  label: 'Password',
+                  required: true,
+                  disabled: false,
+                  hideExpression: '!username'
+              }
+          }, {
+              "key": "love",
+              "type": "input",
+              templateOptions: {
+                  type: 'number',
+                  "label": "How much love?",
+                  "min": 0,
+                  "max": 100,
+                  "maxlength": 4,
+                  "required": true
+              }
+          }, {
+              type: 'input',
+              templateOptions: {
+                  label: 'random number',
+                  type: 'number',
+                  maxlength: 12
+              }
+          }
+      ];
 
-.controller('View1Controller', [
-    function () {
-        var viewModel = this;
+      viewModel.formOptions = {
+          //uniqueFormId: 'myFormId'
+      };
 
-        viewModel.formData = {};
+      viewModel.onSubmit = function() {
+          console.log('form submitted:', viewModel.formData);
+      };
+  };   
+})()
 
-        viewModel.formFields = [
-            {
-                key: 'username',
-                type: 'input',
-                templateOptions: {
-                    type: 'text',
-                    label: 'User Name',
-                    placeholder: 'john smith',
-                    required: true,
-                    description: 'User Name for the system',
-                    minlength: 2
-                }
-            }, {
-                key: 'password',
-                type: 'input',
-                templateOptions: {
-                    type: 'password',
-                    label: 'Password',
-                    required: true,
-                    disabled: false,
-                    hideExpression: '!username'
-                }
-            }, {
-                "key": "love",
-                "type": "input",
-                templateOptions: {
-                    type: 'number',
-                    "label": "How much love?",
-                    "min": 0,
-                    "max": 100,
-                    "maxlength": 4,
-                    "required": true
-                }
-            }, {
-                type: 'input',
-                templateOptions: {
-                    label: 'random number',
-                    type: 'number',
-                    maxlength: 12
-                }
-            }
-        ];
 
-        viewModel.formOptions = {
-            //uniqueFormId: 'myFormId'
-        };
-
-        viewModel.onSubmit = function() {
-            console.log('form submitted:', viewModel.formData);
-        };
-}]);
